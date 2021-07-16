c7d44086b7cfa7e305b89678188876987a006734
chansoo1280 <>
2021-07-16 14:21:54 +0900

    Update data.repository.ts

--- backend/src/etc/data/data.repository.ts
+++ backend/src/etc/data/data.repository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository, Repository } from 'typeorm';
 
-import { Data } from './Data.entity';
+import { Data } from './data.entity';
 
 @EntityRepository(Data)
 export class DataRepository extends Repository<Data> {}
