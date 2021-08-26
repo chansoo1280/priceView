2e83ce586b31c85c3eef3ab6782dd320115cfcea
chansoo1280 <62010067+chansoo1280@users.noreply.github.com>
2021-08-26 20:20:14 +0900

    Update app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '새로운 ci/cd 환경입니다.';
+    return '새로운 ci/cd 환경입니다.\n  환영합니다!';
   }
 }
