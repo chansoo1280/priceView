d5e0d3f8c3f76502b8c8f80dd9e47fc5814d1634
chansoo1280 <>
2021-09-01 18:00:11 +0900

    feat(setting): init dev
    설정 페이지 프론트 작업

--- frontend/pages/setting/index.tsx
+++ frontend/pages/setting/index.tsx
@@ -4,95 +4,103 @@ import { useState } from "react"
 
 // #region Local Imports
 import { ISettingPage, ReduxNextPageContext } from "@Interfaces"
-import { LayoutCode, Title, Select, AlertModal, SettingList, SettingListInner, SettingTitle } from "@Components"
+import { Header, Button, Title, Select, AlertModal, SettingList, SettingListInner, SettingTitle } from "@Components"
+import { useRouter } from "next/router"
 // #endregion Local Imports
 
-const Setting = ({}: ISettingPage.InitialProps) => {
+const Setting = ({}: ISettingPage.InitialProps): JSX.Element => {
+    const router = useRouter()
     const [showDataModal, setShowDataModal] = useState(false)
     const [showPrivacyModal, SetShowPrivacyModal] = useState(false)
     return (
-        <main id="contents" className="l_main">
-            <SettingTitle>
-                <Title as="h2">사용성</Title>
-            </SettingTitle>
-            <SettingList>
-                <SettingListInner>
-                    <Title as="h3">언어</Title>
-                    <Select
-                        setValue={() => {
-                            console.log("hi")
+        <>
+            <Header title="설정">
+                <Button onClick={() => router.back()} icon={<i className="xi-angle-left"></i>}>
+                    <span className="ir">뒤로가기</span>
+                </Button>
+            </Header>
+            <main id="contents" className="l_main">
+                <SettingTitle>
+                    <Title as="h2">사용성</Title>
+                </SettingTitle>
+                <SettingList>
+                    <SettingListInner>
+                        <Title as="h3">언어</Title>
+                        <Select
+                            setValue={() => {
+                                console.log("hi")
+                            }}
+                        >
+                            <option value="">한국어</option>
+                            <option value="">English(영어)</option>
+                            <option value="">日本語(일본어)</option>
+                        </Select>
+                    </SettingListInner>
+                    <SettingListInner>
+                        <Title as="h3">테마</Title>
+                        <Select
+                            setValue={() => {
+                                console.log("hi")
+                            }}
+                        >
+                            <option value="">기본</option>
+                            <option value="">다크</option>
+                            <option value="">베이지</option>
+                        </Select>
+                    </SettingListInner>
+                </SettingList>
+                <SettingTitle>
+                    <Title as="h2">앱 정보</Title>
+                </SettingTitle>
+                <SettingList>
+                    <SettingListInner
+                        onClick={() => {
+                            setShowDataModal(true)
                         }}
                     >
-                        <option value="">한국어</option>
-                        <option value="">English(영어)</option>
-                        <option value="">日本語(일본어)</option>
-                    </Select>
-                </SettingListInner>
-                <SettingListInner>
-                    <Title as="h3">테마</Title>
-                    <Select
-                        setValue={() => {
-                            console.log("hi")
+                        <Title as="h3">데이터 제공</Title>
+                        서울 열린데이터 광장
+                    </SettingListInner>
+                    <SettingListInner>
+                        <Title as="h3">제작자</Title>
+                        Chansoo Kim
+                    </SettingListInner>
+                    <SettingListInner
+                        onClick={() => {
+                            SetShowPrivacyModal(true)
                         }}
                     >
-                        <option value="">기본</option>
-                        <option value="">다크</option>
-                        <option value="">베이지</option>
-                    </Select>
-                </SettingListInner>
-            </SettingList>
-            <SettingTitle>
-                <Title as="h2">앱 정보</Title>
-            </SettingTitle>
-            <SettingList>
-                <SettingListInner
+                        <Title as="h3">개인정보처리방침</Title>
+                    </SettingListInner>
+                    <SettingListInner>
+                        <Title as="h3">버전 정보</Title>
+                        0.0.0.1
+                    </SettingListInner>
+                </SettingList>
+                <AlertModal
                     onClick={() => {
-                        setShowDataModal(true)
+                        setShowDataModal(false)
                     }}
+                    show={showDataModal}
+                    title={"데이터 제공"}
                 >
-                    <Title as="h3">데이터 제공</Title>
-                    서울 열린데이터 광장
-                </SettingListInner>
-                <SettingListInner>
-                    <Title as="h3">제작자</Title>
-                    Chansoo Kim
-                </SettingListInner>
-                <SettingListInner
+                    서울시 생필품 농수축산물 가격 정보API
+                </AlertModal>
+                <AlertModal
                     onClick={() => {
-                        SetShowPrivacyModal(true)
+                        SetShowPrivacyModal(false)
                     }}
+                    show={showPrivacyModal}
+                    title={"개인정보처리방침"}
                 >
-                    <Title as="h3">개인정보처리방침</Title>
-                </SettingListInner>
-                <SettingListInner>
-                    <Title as="h3">버전 정보</Title>
-                    0.0.0.1
-                </SettingListInner>
-            </SettingList>
-            <AlertModal
-                onClick={() => {
-                    setShowDataModal(false)
-                }}
-                show={showDataModal}
-                title={"데이터 제공"}
-            >
-                서울시 생필품 농수축산물 가격 정보API
-            </AlertModal>
-            <AlertModal
-                onClick={() => {
-                    SetShowPrivacyModal(false)
-                }}
-                show={showPrivacyModal}
-                title={"개인정보처리방침"}
-            >
-                본 앱은 사용자의 정보를 수집하지 않습니다.
-            </AlertModal>
-        </main>
+                    본 앱은 사용자의 정보를 수집하지 않습니다.
+                </AlertModal>
+            </main>
+        </>
     )
 }
 Setting.getInitialProps = async (ctx: ReduxNextPageContext): Promise<ISettingPage.InitialProps> => {
     return {
-        layout: LayoutCode.Setting,
         transition: "popup",
     }
 }
--- frontend/src/Components/Atom/Button/index.tsx
+++ frontend/src/Components/Atom/Button/index.tsx
@@ -1,26 +1,88 @@
 // #region Global Imports
-import React from "react"
+import React, { MouseEventHandler, ReactNode } from "react"
 import Link from "next/link"
+import classNames from "classnames"
 // #endregion Global Imports
 
 // #region Local Imports
-import { IButton } from "./Button"
-import { StyledButton } from "./styled"
+import styles from "./Button.module.scss"
 // #endregion Local Imports
-export { StyledButton }
 
-const Button: React.FunctionComponent<IButton.IProps> = (props) => {
-    const { href } = props
+const ButtonTypes = ["default", "primary", "dashed", "link", "text"] as const
+export type ButtonType = typeof ButtonTypes[number]
+const ButtonShapes = ["circle", "round"] as const
+export type ButtonShape = typeof ButtonShapes[number]
+const ButtonHTMLTypes = ["submit", "button", "reset"] as const
+export type ButtonHTMLType = typeof ButtonHTMLTypes[number]
+
+interface BaseButtonProps {
+    children?: ReactNode
+    icon?: React.ReactNode
+    type?: ButtonType
+    shape?: ButtonShape
+    onClick?: MouseEventHandler
+    show?: boolean
+    ghost?: boolean
+    danger?: boolean
+    href?: string
+    loading?: boolean | { delay?: number }
+    className?: string
+    block?: boolean
+    htmlType?: ButtonHTMLType
+}
+type AnchorButtonProps = {
+    href: string
+    target?: string
+    onClick?: React.MouseEventHandler<HTMLElement>
+} & BaseButtonProps &
+    Omit<React.AnchorHTMLAttributes<any>, "type" | "onClick">
+
+type NativeButtonProps = {
+    htmlType?: ButtonHTMLType
+    onClick?: React.MouseEventHandler<HTMLElement>
+} & BaseButtonProps &
+    Omit<React.ButtonHTMLAttributes<any>, "type" | "onClick">
+
+type ButtonProps = Partial<AnchorButtonProps & NativeButtonProps>
+
+type Loading = number | boolean
+const Button = (props: ButtonProps): JSX.Element => {
+    const { href, icon, loading = false, htmlType = "button", type, shape, className, children, block, ...rest } = props
+    const [innerLoading, setLoading] = React.useState<Loading>(!!loading)
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>) => {
+        const { onClick, disabled } = props
+        if (innerLoading || disabled) {
+            e.preventDefault()
+            return
+        }
+        ;(onClick as React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>)?.(e)
+    }
+    const iconType = innerLoading ? "loading" : icon
+    const prefixCls = "btn"
+    const classes = classNames(
+        styles[`${prefixCls}`],
+        {
+            [styles[`${prefixCls}--${type}`]]: type,
+            [styles[`${prefixCls}--${shape}`]]: shape,
+            [styles[`${prefixCls}--icon-only`]]: !children && children !== 0 && !!iconType,
+            [styles[`${prefixCls}--loading`]]: innerLoading,
+            [styles[`${prefixCls}--block`]]: block,
+        },
+        className,
+    )
     return href !== undefined ? (
         <Link href={href}>
-            <StyledButton as={"a"} {...props} />
+            <a {...(rest as AnchorButtonProps)} onClick={handleClick} className={classes}>
+                {icon}
+                {children}
+            </a>
         </Link>
     ) : (
-        <StyledButton {...props} />
+        <button {...(rest as NativeButtonProps)} onClick={handleClick} type={htmlType} className={classes}>
+            {icon}
+            {children}
+        </button>
     )
-}
-Button.defaultProps = {
-    type: "button",
 }
 
 export { Button }
