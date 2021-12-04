a66638f048c02493a0a0955b79737a12ea6ab762
chansoo1280 <chansoo1280@naver.com>
2021-12-04 23:16:17 +0900

    ignore env

--- backend/src/config/config.service.ts
+++ backend/src/config/config.service.ts
@@ -1,12 +1,12 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm'
-import path from 'path'
-import Dotenv from 'dotenv'
+import { TypeOrmModuleOptions } from "@nestjs/typeorm"
+import path from "path"
+import Dotenv from "dotenv"
 
 const envPath: string = (function () {
     if (process.env.pm_cwd) {
-        return path.resolve(process.env.pm_cwd, '.env')
+        return path.resolve(process.env.pm_cwd, ".env")
     }
-    return path.resolve(process.cwd(), '.env')
+    return path.resolve(process.cwd(), ".env")
 })()
 Dotenv.config({ path: envPath })
 
@@ -17,8 +17,8 @@ class ConfigService {
         const value =
             this.env[
                 // eslint-disable-next-line no-nested-ternary
-                this.env.NODE_ENV === 'test' ? `${key}_TEST` : this.env.NODE_ENV === 'development' ? `${key}_DEV` : key
-            ] || ''
+                this.env.NODE_ENV === "test" ? `${key}_TEST` : this.env.NODE_ENV === "development" ? `${key}_DEV` : key
+            ] || ""
 
         if ((!value && throwOnMissing) || value === undefined) {
             throw new Error(`config error - missing env.${key} =  ${JSON.stringify(this.env)}`)
@@ -33,19 +33,19 @@ class ConfigService {
 
     public isProduction() {
         const mode = this.env.MODE
-        return mode !== 'DEV'
+        return mode !== "DEV"
     }
 
     public getTypeOrmConfig(): TypeOrmModuleOptions {
         return {
-            type: 'mssql',
-            host: this.getValue('MSSQL_HOST'),
-            port: Number(this.getValue('MSSQL_PORT')),
-            username: this.getValue('MSSQL_USER'),
-            password: this.getValue('MSSQL_PASSWORD'),
-            database: this.getValue('MSSQL_DATABASE'),
+            type: "mssql",
+            host: this.getValue("MSSQL_HOST"),
+            port: Number(this.getValue("MSSQL_PORT")),
+            username: this.getValue("MSSQL_USER"),
+            password: this.getValue("MSSQL_PASSWORD"),
+            database: this.getValue("MSSQL_DATABASE"),
 
-            entities: JSON.parse(this.getValue('MSSQL_ENTITIES')),
+            entities: JSON.parse(this.getValue("MSSQL_ENTITIES")),
 
             logging: true, // (this.env.NODE_ENV === 'test'),
             synchronize: false, // (this.env.NODE_ENV === 'test'),
@@ -53,19 +53,19 @@ class ConfigService {
                 encrypt: false,
                 enableArithAbort: true,
             },
-            requestTimeout: 0,
-            connectionTimeout: 0,
+            requestTimeout: 0, // for timeout setting
+            connectionTimeout: 0, // for timeout setting
             ssl: this.isProduction(),
         }
     }
 }
 
 const configService = new ConfigService(process.env).ensureValues([
-    'MSSQL_HOST',
-    'MSSQL_PORT',
-    'MSSQL_USER',
-    'MSSQL_PASSWORD',
-    'MSSQL_DATABASE',
+    "MSSQL_HOST",
+    "MSSQL_PORT",
+    "MSSQL_USER",
+    "MSSQL_PASSWORD",
+    "MSSQL_DATABASE",
 ])
 
 export { configService }
