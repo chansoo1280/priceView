376d297bea9413b8861ce2420095f3eaa5cdc6f6
chansoo1280 <chansoo1280@naver.com>
2021-12-05 00:33:51 +0900

    Fix

--- backend/src/count/count.controller.ts
+++ backend/src/count/count.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Query, Req, Res } from '@nestjs/common'
-import { M_GU, M_GU_CODE_LIST, M_TYPE_CODE_LIST, ITEM_OBJ, M_TYPE_DATA } from '@Definitions'
-import handleResult, { ResCode } from 'src/common/handleResult'
-import { Price } from 'src/price/price.entity'
-import { PriceService } from 'src/price/price.service'
-import { Response, Request } from 'express'
-import { SECRET } from 'src/common/constants'
-import { CountService } from './count.service'
-import { SelectCountDto } from './dto/select-count.dto'
+import { Controller, Get, Query, Req, Res } from "@nestjs/common"
+import { M_GU, M_GU_CODE_LIST, M_TYPE_CODE_LIST, ITEM_OBJ, M_TYPE_DATA } from "@Definitions"
+import handleResult, { ResCode } from "src/common/handleResult"
+import { Price } from "src/price/price.entity"
+import { PriceService } from "src/price/price.service"
+import { Response, Request } from "express"
+import { SECRET } from "src/common/constants"
+import { CountService } from "./count.service"
+import { SelectCountDto } from "./dto/select-count.dto"
 
-@Controller('count')
+@Controller("count")
 export class CountController {
     constructor(private countService: CountService, private priceService: PriceService) {}
 
@@ -20,21 +20,21 @@ export class CountController {
         @Res() res: Response,
     ): Promise<Response> {
         if (req.headers.secret !== SECRET) return handleResult(ResCode.Unauthorized, res)
-        const insertCount = async (P_YEAR_MONTH: Price['P_YEAR_MONTH'], A_SEQ: number, A_UNIT: string[]) => {
+        const insertCount = async (P_YEAR_MONTH: Price["P_YEAR_MONTH"], A_SEQ: number, A_UNIT: readonly string[]) => {
             console.log(A_SEQ)
             const result = await this.priceService.getCnt({
-                name: ITEM_OBJ[A_SEQ].A_NAME_DATA,
+                name: ITEM_OBJ[A_SEQ as keyof typeof ITEM_OBJ].A_NAME_DATA,
                 A_UNIT,
                 P_YEAR_MONTH,
             })
             for (let i = 0; i < M_TYPE_CODE_LIST.length; i += 1) {
                 const typeList = result.filter((priceInfo: Price) => {
-                    if (M_TYPE_CODE_LIST[i] === '') return true
+                    if (M_TYPE_CODE_LIST[i] === "") return true
                     return priceInfo.M_TYPE_CODE === M_TYPE_CODE_LIST[i]
                 })
                 for (let j = 0; j < M_GU_CODE_LIST.length; j += 1) {
                     const insertList = typeList.filter((priceInfo: Price) => {
-                        if (M_GU_CODE_LIST[j] === '') return true
+                        if (M_GU_CODE_LIST[j] === "") return true
                         return priceInfo.M_GU_CODE === M_GU_CODE_LIST[j]
                     })
                     // console.log(M_TYPE_CODE_LIST[i], M_GU_CODE_LIST[j], insertList.length)
@@ -45,7 +45,7 @@ export class CountController {
                         }
                         return price
                     })()
-                    const C_CODE = [String(A_SEQ), P_YEAR_MONTH, M_TYPE_CODE_LIST[i], M_GU_CODE_LIST[j]].join('_')
+                    const C_CODE = [String(A_SEQ), P_YEAR_MONTH, M_TYPE_CODE_LIST[i], M_GU_CODE_LIST[j]].join("_")
                     // eslint-disable-next-line no-await-in-loop
                     const cntInfo = await this.countService.getCount(C_CODE)
                     if (cntInfo !== ResCode.NoContent) {
@@ -61,14 +61,14 @@ export class CountController {
                         await this.countService.createCount({
                             C_CODE,
                             A_SEQ: String(A_SEQ),
-                            A_NAME: ITEM_OBJ[A_SEQ].A_NAME_DATA,
+                            A_NAME: ITEM_OBJ[A_SEQ as keyof typeof ITEM_OBJ].A_NAME_DATA,
                             A_UNIT: A_UNIT[0],
                             P_YEAR: P_YEAR_MONTH.slice(0, 4),
                             P_YEAR_MONTH,
                             M_TYPE_CODE: M_TYPE_CODE_LIST[i],
-                            M_TYPE_NAME: M_TYPE_DATA[M_TYPE_CODE_LIST[i]] || '',
+                            M_TYPE_NAME: M_TYPE_DATA[M_TYPE_CODE_LIST[i]] || "",
                             M_GU_CODE: M_GU_CODE_LIST[j],
-                            M_GU_NAME: M_GU[M_GU_CODE_LIST[j]] || '',
+                            M_GU_NAME: M_GU[M_GU_CODE_LIST[j]] || "",
                             length: insertList.length,
                             AVER_VAL: A_TOTAL_PRICE / insertList.length || 0,
                         })
@@ -79,14 +79,14 @@ export class CountController {
         console.log(P_YEAR_MONTH)
         await Object.keys(ITEM_OBJ).reduce(async (previousPromise: any, A_SEQ) => {
             await previousPromise
-            return insertCount(P_YEAR_MONTH, Number(A_SEQ), ITEM_OBJ[Number(A_SEQ)].A_UNIT)
+            return insertCount(P_YEAR_MONTH, Number(A_SEQ), ITEM_OBJ[Number(A_SEQ) as keyof typeof ITEM_OBJ].A_UNIT)
         }, Promise.resolve())
         // await insertCount("2020-02", Number("312"), ["1개"])
-        console.log('done')
+        console.log("done")
         return handleResult(ResCode.OK, res)
     }
 
-    @Get('/info')
+    @Get("/info")
     async getStatistics(
         @Query() { A_SEQS, P_YEAR_MONTH, M_GU_CODE }: SelectCountDto,
         @Res() res: Response,
@@ -97,8 +97,8 @@ export class CountController {
                 return str
             }
             const list = []
-            let year = Number(P_YEAR_MONTH.split('-')[0])
-            let month = Number(P_YEAR_MONTH.split('-')[1])
+            let year = Number(P_YEAR_MONTH.split("-")[0])
+            let month = Number(P_YEAR_MONTH.split("-")[1])
             for (let i = 0; i < 4; i += 1) {
                 list.push(`${fillZero(String(year))}-${fillZero(String(month))}`)
                 if (month !== 1) {
@@ -110,7 +110,7 @@ export class CountController {
             }
             return list
         })()
-        const A_SEQ_LIST = A_SEQS.split(', ')
+        const A_SEQ_LIST = A_SEQS.split(", ")
         const result = await this.countService.getStatistics({
             A_SEQ_LIST,
             P_YEAR_MONTH_LIST,
--- backend/src/price/price.controller.ts
+++ backend/src/price/price.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common'
-import handleResult, { ResCode } from 'src/common/handleResult'
-import { Response, Request } from 'express'
-import { SECRET } from 'src/common/constants'
-import { ITEM_OBJ } from '@Definitions'
-import { PriceService } from './price.service'
-import { SelectCntPriceDto } from './dto/select_cnt-price.dto'
-import { InitPriceDto } from './dto/init-count.dto'
+import { Controller, Get, Param, Query, Req, Res } from "@nestjs/common"
+import handleResult, { ResCode } from "src/common/handleResult"
+import { Response, Request } from "express"
+import { SECRET } from "src/common/constants"
+import { ITEM_OBJ } from "@Definitions"
+import { PriceService } from "./price.service"
+import { SelectCntPriceDto } from "./dto/select_cnt-price.dto"
+import { InitPriceDto } from "./dto/init-count.dto"
 
-@Controller('price')
+@Controller("price")
 export class PriceController {
     constructor(private priceService: PriceService) {}
 
@@ -22,7 +22,7 @@ export class PriceController {
         const insertData = async (page: number) => {
             const result = await this.priceService.findAll(P_YEAR_MONTH, page)
             const resCode = result.ListNecessariesPricesService?.RESULT?.CODE
-            if (resCode !== 'INFO-000') return
+            if (resCode !== "INFO-000") return
 
             const totalList = result.ListNecessariesPricesService.row
             const step = 100
@@ -35,7 +35,7 @@ export class PriceController {
         }
         const result = await this.priceService.getLen(P_YEAR_MONTH)
         const resCode = result.ListNecessariesPricesService?.RESULT?.CODE
-        if (resCode !== 'INFO-000') return handleResult(ResCode.NoContent, res) // 내용이 없음
+        if (resCode !== "INFO-000") return handleResult(ResCode.NoContent, res) // 내용이 없음
 
         const listTotalCnt = result.ListNecessariesPricesService?.list_total_count
         const totalLen = Math.ceil(listTotalCnt / 1000) // 1000개씩 돌리기
@@ -48,13 +48,13 @@ export class PriceController {
         return handleResult(ResCode.OK, res)
     }
 
-    @Get('/:M_SEQ')
+    @Get("/:M_SEQ")
     async getCnt(
         @Param() { M_SEQ }: { M_SEQ: string },
         @Query() { P_YEAR_MONTH }: SelectCntPriceDto,
         @Res() res: Response,
     ): Promise<Response> {
-        const { A_NAME_DATA, A_UNIT } = ITEM_OBJ[Number(M_SEQ)]
+        const { A_NAME_DATA, A_UNIT } = ITEM_OBJ[Number(M_SEQ) as keyof typeof ITEM_OBJ]
         const result = await this.priceService.getCnt({
             name: A_NAME_DATA,
             A_UNIT,
